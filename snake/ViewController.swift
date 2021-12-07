b01c9bde6a2e486704512ad093a064ff8767bed9
anthonyBorges98 <aborges1@mail.sfsu.edu>
2021-12-06 22:36:59 -0800

    Initial Commit

---
+++ snake/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  snake
+//
+//  Created by Anthony Borges on 12/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
