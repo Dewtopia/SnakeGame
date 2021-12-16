558937457a10e97d3415af04c951987272afb60e
Grayson <55421761+Dewtopia@users.noreply.github.com>
2021-12-16 12:07:48 -0800

    Added death function and more

--- SnakeGameTests/SnakeGameBackground.swift
+++ SnakeGameTests/SnakeGameBackground.swift
@@ -13,16 +13,6 @@ class SnakeGameBackgroundTest: XCTestCase {
     func testPrintBackground() {
         var background = SnakeGameBackground()
         background.snake.append(SnakeCell(column: 2, row: 1))
-        background.snake.append(SnakeCell(column: 3, row: 1))
-        background.snake.append(SnakeCell(column: 3, row: 2))
-        background.snake.append(SnakeCell(column: 3, row: 3))
-        print(background)
-        XCTAssertEqual(2, background.snake[0].column)
-        XCTAssertEqual(1, background.snake[0].row)
-        background.moveLeft()
-        XCTAssertEqual(1, background.snake[0].column)
-        XCTAssertEqual(1, background.snake[0].row)
-        background.moveLeft()
         print(background)
     }
 }
