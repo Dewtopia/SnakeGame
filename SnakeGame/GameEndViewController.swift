25c889aadfd14191b4dccc3c1682f3135608a541
Grayson <55421761+Dewtopia@users.noreply.github.com>
2021-12-16 18:05:59 -0800

    second VC

---
+++ SnakeGame/GameEndViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GameEndViewController.swift
+//  SnakeGame
+//
+//  Created by JPL-ST-SPRING2021 on 12/16/21.
+//  Copyright © 2021 csc. All rights reserved.
+//
+
+import UIKit
+
+class GameEndViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+}
